e958c6d45636ec6b580b31786abd38651efef03a
Agneedh Roy <agneedhroy@gmail.com>
2025-10-05 15:57:15 +0530

    Update streamlit_app.py
    added changes to smoothiefroot call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -42,9 +42,11 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
 
+if ingredients_list:
+  ingredients_string = ''
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ' '
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
-#New Section to Displya SmoothieFruit Nutrition Information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
